3b20749f11e9b031d0c4de87c38ba806faa1feb4
lindsayPeeters <lindsay.99.peeters@gmail.com>
2023-05-03 16:43:58 +0200

    feat: header call

---
+++ screens/call.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Call</title>
+    <link rel="stylesheet" href="../style/style.css" type="text/css">
+</head>
+<body>
+    <header id="headerCall">
+        <div>
+            <h1>Sofie</h1>
+            <p>is calling...</p>
+        </div>    
+    </header>
+</body>
+</html>
