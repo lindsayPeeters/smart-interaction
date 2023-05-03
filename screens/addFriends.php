080e6a6c83b3e617d9daa0c5a093d2bd80579956
lindsayPeeters <lindsay.99.peeters@gmail.com>
2023-05-03 13:11:48 +0200

    feat: navigation to addFriends screen

---
+++ screens/addFriends.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>add your friends</title>
+    <link rel="stylesheet" href="../style/style.css" type="text/css">
+</head>
+<body>
+    <header>
+        <a href="./home.php">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="backbtn">
+                <mask id="mask0_49_89" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+                    <rect width="24" height="24" fill="#D9D9D9"/>
+                </mask>
+                <g mask="url(#mask0_49_89)">
+                    <path d="M12 20L4 12L12 4L13.425 5.4L7.825 11H20V13H7.825L13.425 18.6L12 20Z" fill="#EEF3F7"/>
+                </g>
+            </svg>
+        </a>
+        <h1>Add friends</h1>
+    </header>
+</body>
+</html>
