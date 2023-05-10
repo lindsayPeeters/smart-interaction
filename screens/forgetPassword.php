ee30e4d200c0129503fc7223996bca4dd923bb52
lindsayPeeters <lindsay.99.peeters@gmail.com>
2023-05-10 15:48:47 +0200

    feat: forgetPassword.php

--- screens/forgetPassword.php
+++ screens/forgetPassword.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>forget password</title>
+    <title>forgot password</title>
     <link rel="stylesheet" href="../style/style.css" type="text/css">
 </head>
 <body>
+<h1>Forget password</h1>
+<section id="section-login">
+    <form action="" method="POST">
+        <label for="newP">New password</label><br>
+        <input type="password" id="newP" name="newP"><br><br>
+        <label for="password">Repeat password</label><br>
+        <input type="password" id="password" name="password"><br><br>
+        <button type="submit" class="purpleBtn"><a href="./home.php">Log in</a></button><br>
+    </form>
+</section>
 </body>
 </html>
